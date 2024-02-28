a8fcbaf5733c5266ada087c1c77b97ab0206c838
patricio-jp <patojimenezpaz@gmail.com>
2024-02-28 00:30:32 -0300

    Initial commit. New NestJS Proyect with Swagger

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('JyC Amoblamientos')
+    .setDescription('API para JyC Amoblamientos')
+    .setVersion('1.0')
+    .addTag('jyc')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
